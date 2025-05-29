692738e113a1aab4f7899494a3964d48a3f72587
Yogesh P <yogeshpanneerselvam28@gmail.com>
2025-05-29 14:00:28 +0530

    Update main.tf

--- cloud web app/terraform/main.tf
+++ cloud web app/terraform/main.tf
@@ -21,29 +21,33 @@ resource "aws_iam_role" "ec2_role" {
   })
 }
 
-resource "aws_iam_policy" "s3_read_policy" {
-  name = "s3_read_policy"
+resource "aws_iam_policy" "s3_read_write_policy" {
+  name = "s3_read_write_policy"
 
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = [
-        "s3:GetObject",
-        "s3:ListBucket"
-      ]
-      Resource = [
-        "arn:aws:s3:::${var.bucket_name}",
-        "arn:aws:s3:::${var.bucket_name}/*"
-      ]
-    }]
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:PutObject"
+        ]
+        Resource = [
+          "arn:aws:s3:::${var.bucket_name}",        # bucket itself (for list)
+          "arn:aws:s3:::${var.bucket_name}/*"       # all objects in bucket
+        ]
+      }
+    ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "attach_s3_read_policy" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = aws_iam_policy.s3_read_policy.arn
+resource "aws_iam_user_policy_attachment" "attach_s3_policy" {
+  user       = aws_iam_user.jenkins_user.name
+  policy_arn = aws_iam_policy.s3_read_write_policy.arn
 }
+
 
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = "ec2_instance_profile"
